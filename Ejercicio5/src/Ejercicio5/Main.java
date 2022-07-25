ddd50079367f51bba271bc61cd7bf49671745edf
Enrique Golpe <enriquegolpe@hotmail.com>
2022-07-25 19:17:15 +0200

    ejercicio5

---
+++ Ejercicio5/src/Ejercicio5/Main.java
@@ -0,0 +1,34 @@
+package Ejercicio5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //núnmeros
+        int variable1 = 10;
+        System.out.println(variable1);
+        double variable2 = 19.03d;
+        System.out.println(
+                variable2
+        );
+        float variable3 = 9.9f;
+        System.out.println(variable3);
+        long variable4 = 10000;
+        System.out.println(variable4);
+
+
+        byte variable6 = 4;
+        System.out.println(variable6);
+        short variable7 = 5;
+        System.out.println(variable7);
+
+        //Booleanos
+        boolean variable8 = true;
+        System.out.println(variable8);
+        boolean variable9 = false;
+        System.out.println(variable9);
+
+        //texto
+        String variable5 = "Hola Mundo";
+        System.out.println(variable5);
+    }
+}
