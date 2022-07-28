bff5dbfc6c7959de8d1880e2bf432ddeaa09b1b7
Enrique Golpe <enriquegolpe@hotmail.com>
2022-07-28 13:34:23 +0200

    ejercicio7

---
+++ ejercicio7/src/nameconcat/Name.java
@@ -0,0 +1,12 @@
+package nameconcat;
+
+public class Name {
+    public static void main(String[] args) {
+        String[] names = new String[]{"Enrique", "juan", "Javier"};
+
+        for(int i = 0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
+    }
+
+}
